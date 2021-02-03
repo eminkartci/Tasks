ffaf0aab89a0108a7640aff7463df53dd13a1bae
Metin Köker <metin.koker@ozu.edu.tr>
2021-02-03 20:14:59 +0300

    Input alma kısmı daha açık hale getirildi

--- Grading_Program.java
+++ Grading_Program.java
@@ -1,28 +1,29 @@
-
 import java.util.*;
 
 public class Grading_Program {
 	public static void main(String[] args) {
-		
 		Scanner console = new Scanner(System.in);
-		System.out.print("Please type midterm 1: \n");
+
+		System.out.print("Type the first Midterm:");
 		float ExamGrade1 = console.nextInt();
 
 		if (ExamGrade1 < 0 || ExamGrade1 > 100) {
 			System.out.print("Please try again. Grade should be in [0,100] interval \n:");
 			ExamGrade1 = console.nextInt();
 		}
-
+		System.out.print("Type the second Midterm:");
 		float ExamGrade2 = console.nextInt();
 		if (ExamGrade2 < 0 || ExamGrade2 > 100) {
 			System.out.print("Please try again. Grade should be in [0,100] interval \n:");
 			ExamGrade2 = console.nextInt();
 		}
+		System.out.print("Type the Final:");
 		float ExamGrade3 = console.nextInt();
 		if (ExamGrade3 < 0 || ExamGrade3 > 100) {
 			System.out.print("Please try again. Grade should be in [0,100] interval \n:");
 			ExamGrade3 = console.nextInt();
 		}
+		console.close();
 		float average = (ExamGrade1 + ExamGrade2 + ExamGrade3) / 3;
 
 		System.out.println("First Midterm Grade : " + ExamGrade1);
