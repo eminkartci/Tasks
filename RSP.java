38e1d876507b2f4a9111cd8caa828e953dd8fc7f
eminkartci <emin.kartci@ozu.edu.tr>
2021-02-09 22:54:20 +0300

    hello world is written

---
+++ RSP.java
@@ -0,0 +1,17 @@
+
+
+class RSP {
+
+    /*
+
+    Author : Emin Kartci
+    Date   : 9 Feb 2021
+    GitHub : eminkartci
+    
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello Emin !!");
+    }
+
+}
