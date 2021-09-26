db1fa618496c386996a46569aeacf023b498a28b
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-26 20:31:53 +0300

    Game Class is added

--- Task2/RockPaperScrissors.py
+++ Task2/RockPaperScrissors.py
@@ -4,22 +4,36 @@ class Player:
     # Constructor
     def __init__(self,id) -> None:
         self.id         = id
+        self.selection  = None
         self.get_nickname()
 
     def __str__(self) -> str:
         return f"""
 --- PLAYER {self.id}
-| Nickname: {self.nickname}
+| Nickname : {self.nickname}
+| Selection: {self.selection}
         """
 
     # Behaviours
     def get_nickname(self):
         self.nickname = input("Nickname: ")
 
+    def select(self):
+        self.selection = int(input("* 1 -> Rock(🪨)\n* 2 -> Paper(📰)\n* 3 -> Scissors(️️✂️))\nSelect: "))
 
-p1 = Player(1)
-p2 = Player(2)
+class Game():
 
+    def __init__(self):
+        self.p1 = Player(1)
+        self.p2 = Player(2)
 
-print(p1)
-print(p2)
+    def play(self):
+        self.p1.select()
+        self.p2.select()
+
+    def print_players(self):
+        print(self.p1)
+        print(self.p2)
+
+    def winner(self):
+        pass
