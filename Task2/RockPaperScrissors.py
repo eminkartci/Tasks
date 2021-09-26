a37e80599d21eb418e57020f58b610453cdfcaaf
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-26 20:38:36 +0300

    Game object is improved

--- Task2/RockPaperScrissors.py
+++ Task2/RockPaperScrissors.py
@@ -31,9 +31,35 @@ class Game():
         self.p1.select()
         self.p2.select()
 
+        self.winner()
+
     def print_players(self):
         print(self.p1)
         print(self.p2)
 
     def winner(self):
-        pass
+        # tie
+        if self.p1.selection == self.p2.selection:
+            print("There is no winner! It's draw.")
+        else:
+            if self.p1.selection == 1: # P1 Rock
+                if self.p2.selection == 2: # P2 WINNER
+                    print(f"Player {self.p2.nickname} is the winner !!!")
+                else:
+                    print(f"Player {self.p1.nickname} is the winner !!!")
+            elif self.p1.selection == 2: # P1 Paper
+                if self.p2.selection == 1: # P1 WINNER
+                    print(f"Player {self.p1.nickname} is the winner !!!")
+                else:
+                    print(f"Player {self.p2.nickname} is the winner !!!")
+            elif self.p1.selection == 3: # P1 Scissors 
+                if self.p2.selection == 2: # P1 WINNER
+                    print(f"Player {self.p1.nickname} is the winner !!!")
+                else:
+                    print(f"Player {self.p2.nickname} is the winner !!!")
+
+
+game = Game()
+
+game.play()
+
