b69785b19646d674228ae018cbc2bdc58a9b7256
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-26 20:17:12 +0300

    player class is constructed

---
+++ Task2/RockPaperScrissors.py
@@ -0,0 +1,13 @@
+
+class Player:
+
+    # Constructor
+    def __init__(self,id,nickname) -> None:
+        self.id         = id
+        self.nickname   = nickname
+
+
+p1 = Player(1,"eminkartci")
+p2 = Player(1,"denizucgun")
+
+print(p1)
