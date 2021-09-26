bfc982ba7190203feeb40afa030136ff51a848e3
eminkartci <emin.kartci@ozu.edu.tr>
2021-09-26 20:19:52 +0300

    Playet toString method is added

--- Task2/RockPaperScrissors.py
+++ Task2/RockPaperScrissors.py
@@ -6,8 +6,14 @@ class Player:
         self.id         = id
         self.nickname   = nickname
 
+    def __str__(self) -> str:
+        return f"""
+--- PLAYER {self.id}
+| Nickname: {self.nickname}
+        """
+
 
 p1 = Player(1,"eminkartci")
-p2 = Player(1,"denizucgun")
+p2 = Player(2,"denizucgun")
 
-print(p1)
+print(p2)
