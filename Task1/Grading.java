9d55569954024a58f144a73e2d6d1a5a3100ab53
borasimsek <bora.simsek@ozu.edu.tr>
2022-02-20 15:51:45 +0300

    Letter Grades added

--- Task1/Grading.java
+++ Task1/Grading.java
@@ -5,31 +5,60 @@ public class Grading {
         find_average();
     }
     public static void find_average() {
+        double midterm1 = 0;
+        double midterm2 = 0;
+        double finalExam    = 0;
+        double avg       = 0;
+        char   letter         = 'F';
 
         //Get the inputs from the user
         Scanner input1 = new Scanner(System.in);
         System.out.print("Midterm 1 grade: ");
-        int midterm1 = input1.nextInt();
+         midterm1 = input1.nextInt();
 
         
         System.out.print("\nMidterm 2 grade: ");
-        int midterm2 = input1.nextInt();
+         midterm2 = input1.nextInt();
 
        
         System.out.print("\nFinal grade: ");
-        int finalExam = input1.nextInt();
+         finalExam = input1.nextInt();
 
         input1.close();
         //Calculate the average
-        double avg = (midterm1+midterm2+finalExam)/3;
-
+         avg = (midterm1+midterm2+finalExam)/3;
+        
+        if (avg >=90 ) {
+            letter ='A';
+            
+        }
+        else if (avg <90 && avg >=80 ) {
+            letter ='B';
+            
+        }
+        else if (avg >=70 && avg <80 ) {
+            letter ='C';
+            
+        }
+        else if (avg <70 && avg >=60 ) {
+            letter ='D';
+            
+            
+        }
+        else{
+            letter ='F';
+            return;
+        }
+        
+        
         // Deneme Commıt
 
         String outputStr =  "\n****** RESULTS ******\n" +
                             "Midterm 1 : "+ midterm1 +"\n" +
                             "Midterm 2 : "+ midterm2 +"\n" +
                             "finalExam : "+ finalExam +"\n" +
-                            "Average   : "+ avg+"\n";
+                            "Average   : "+ avg+"\n"+
+                            "Letter    : " + letter ;
 
         //display the output
         System.out.println(outputStr);
