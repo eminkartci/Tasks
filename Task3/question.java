7083f0536b4b413163b231c0e7c6a0e0739159ed
borasimsek <bora.simsek@ozu.edu.tr>
2022-02-25 01:11:54 +0300

    Simple organisation without psuedo

---
+++ Task3/question.java
@@ -0,0 +1,113 @@
+public class question {
+    public static void main(String[] args) {
+        
+
+
+    }
+
+
+}
+
+/* All question
+Title: Bank Account
+
+1. Create a "Customer" class
+
+    Properties
+    ------------------------
+    a. ID
+    b. Name
+    c. Surname
+    d. Birth Year
+    e. TCKN
+    f. Balance
+
+    Constructor
+    ------------------------
+
+    Behaviours
+    ------------------------
+    a. upload_money
+    b. withdraw_money
+    c. take_credit
+    d. show_customer
+    e. save_customer
+    
+
+2. Behaviour Explanations
+   
+   a -> customer can upload money to his/her balance
+   b -> customer can withdraw money but s/he cannot withdraw greater than his/her account.
+   c -> they can take credit
+   d -> when you call this function the output should look like:
+   
+   ----------------> CUSTOMER {ID} <----------------
+   Name         : ---
+   Surname      : ---
+   Birth Year   : ---
+   TCKN         : ---
+   Balance      : ---
+
+   e -> when you call this function the data of the customer will be saved as txt file
+
+3. Write a driver program
+
+    a. Create a customer
+    b. Get Name Surname ...
+    c. Show the customer
+    d. Deposit 100 tl
+    e. Show the customer
+    f. Withdraw 40 tl
+    g. Show the customer
+    h. Withdraw 170 tl
+    i. Take 100 tl credit
+    j. Show the customer
+*/
+
+class Customer {
+
+    /*Properties
+    ------------------------
+    a. ID
+    b. Name
+    c. Surname
+    d. Birth Year
+    e. TCKN
+    f. Balance
+*/
+
+    /*Constructor
+    ------------------------
+*/
+
+
+    //Behaviours
+
+}
+
+class bank_operations {
+    //Properties
+    
+
+    /*Constructor
+    ------------------------
+*/
+
+
+    /*Behaviours 
+    ------------------------
+    a. upload_money
+    b. withdraw_money
+    c. take_credit
+    d. show_customer
+    e. save_customer
+*/
+
+
+
+}
+
+
+
+
+
